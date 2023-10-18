328148df1d35cb014f442ce7ed95e97d36d95a14
micaelygusmao <micaely.gusmao@estudante.ifes.edu.br>
2023-10-18 09:10:09 -0300

    Estrutura inicial

---
+++ src/main/java/com/ifes/trabalhodw/TrabalhodwApplication.java
@@ -0,0 +1,13 @@
+package com.ifes.trabalhodw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhodwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhodwApplication.class, args);
+	}
+
+}
