632e1aa86a87f41f65dc875e21f7f0431cc05f89
Richard l <richard.nunes@morpheusjr.com>
2023-10-21 12:30:10 -0300

    Entidade Operações para Épico E História de Usuários

---
+++ src/main/java/com/ifes/trabalhodw/model/entity/OperacaoHistoriaUsuario.java
@@ -0,0 +1,26 @@
+package com.ifes.trabalhodw.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OperacaoHistoriaUsuario {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    private UUID id;
+    private String nome;
+
+     @ManyToOne
+     @JoinColumn(name = "historiasUsuario_id")
+    private HistoriaDeUsuario historiasUsuario;
+}
