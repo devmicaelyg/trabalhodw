10314e6ca2d2ff8f2788b4a296b04c1d6cdb4305
Richard l <richard.nunes@morpheusjr.com>
2023-10-28 19:59:29 -0300

    Adcionado descrição para projeto

--- src/main/java/com/ifes/trabalhodw/model/dto/ProjetoDto.java
+++ src/main/java/com/ifes/trabalhodw/model/dto/ProjetoDto.java
@@ -1,7 +1,5 @@
 package com.ifes.trabalhodw.model.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.ifes.trabalhodw.model.entity.Projeto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,7 +9,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ProjetoDto {
     private String nome;
-    @JsonIgnore
     private String descricao;
 
 
